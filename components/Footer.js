9629d9f95be7d9315dbc6fb5a1ffb3e547c24ae6
Jake Ward <thegaminggeek362@gmail.com>
2020-03-07 18:46:13 +0100

    Add statuspage notice

--- components/Footer.js
+++ components/Footer.js
@@ -8,8 +8,8 @@ const Footer = () => (
                 Website Made by <a href="https://nystrex.com"><span className="fire">Hadi Ka (Nystrex)</span></a>.
             </div>
         </div>
+        <script src="https://fhrcp0477jwt.statuspage.io/embed/script.js"></script>
     </footer>
-
 );
 
 export default Footer;
