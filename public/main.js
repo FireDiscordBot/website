8ab621fecc2fee1320ec5fc0c99abaa02a5771f9
Jake Ward <thegaminggeek362@gmail.com>
2020-03-08 22:29:15 +0100

    Realtime description (fire guild updates description whenever guild count updates)

--- public/main.js
+++ public/main.js
@@ -4,6 +4,8 @@ eventSrc.onmessage = function(res) {
     let guild = data.id;
     let members = document.getElementById(`${guild}-members`);
     let online = document.getElementById(`${guild}-online`);
+    let desc = document.getElementById(`${guild}-desc`);
     members.innerHTML = `<strong>${data.members}</strong>&nbsp;Members`;
     online.innerHTML = `<strong>${data.online}</strong>&nbsp;Online`;
+    desc.innerHTML = `<span>${data.description}</span>`
 };
