7f4d3ad8a0da68f8c254299cdf65d743343fde48
Jake Ward <thegaminggeek362@gmail.com>
2020-03-26 00:27:54 +0100

    memes (https://inv.wtf/covid)

--- pages/discover.js
+++ pages/discover.js
@@ -47,6 +47,19 @@ const Discover = props => {
                         </div>
                     </div>
                     <br />
+                    <div className="container-RHl2Ju">
+                        <div className="content-2CqL_a">
+                            <div className="icon-3o6xvg avatar-JMNKnp iconSizeMedium-2OqPjI iconInactive-98JN5i" role="button" style={{backgroundImage: 'url(https://cdn.discordapp.com/icons/670375966232674336/6b8c377dd199b30bd37b93923762c34f.png?size=256)'}} />
+                                <div className="text-2IH2ZS">
+                                    <h3 className="wrapper-1sSZUt base-1x0h_U size16-1P40sf">Stay safe and informed</h3>
+                                    <div className="colorHeaderSecondary-3Sp3Ft size14-e6ZScH">Visit the community-run Coronavirus Discord to talk about COVID-19, and head to <a className="anchor-3Z-8Bb anchorUnderlineOnHover-2ESHQB" href="https://www.cdc.gov/coronavirus/2019-nCoV/index.html" rel="noreferrer noopener" target="_blank">CDC.gov</a> for more information.</div>
+                               </div>
+                           </div>
+                       <button type="button" className="button-2RuHlm button-38aScr lookFilled-1Gx00P colorBrand-3pXr91 sizeMedium-1AC_Sl grow-q77ONN" onClick={()=> window.open("https://inv.wtf/covid", "_blank")}>
+                           <div className="contents-18-Yxp">Visit COVID-19 Discord</div>
+                       </button>
+                    </div>
+                    <br />
                     <Servers servers={currentServers} />
                     <br />
                     <Pagination
