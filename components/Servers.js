4c3710f8579d91b8f60010135c9a1650ece858c2
Hadi Ka (Nystrex) <alcacounihadi@gmail.com>
2020-04-01 02:58:19 -0400

    e

--- components/Servers.js
+++ components/Servers.js
@@ -5,65 +5,13 @@ const Servers = ({ servers }) => {
         <div className={"row d-flex align-items-center justify-content-center"}>
             {servers.map((server, i) => (
                 <div className={"server-card"} key={i} onClick={() => window.open(server.vanity, "_blank")}>
-                    <div className={"server-header"}>
-                        <img className={"server-splash"} src={`${server.splash}`} />
-                        <img className={"server-icon"} src={`${server.icon}`} />
-                    </div>
-                    <div className={"server-body text-left"}>
-                        <div className={"server-title"}>
-                            {server.badge === null ? (
-                                ""
-                            ) : server.badge === "partner" ? (
-                                <img
-                                    height="20px"
-                                    src={`https://cdn.discordapp.com/emojis/647415387843198988.png?v=1`}
-                                    style={{ marginRight: 5 + "px" }}
-                                    title="Partnered Server"
-                                ></img>
-                            ) : server.badge === "verified" ? (
-                                <img
-                                    height="20px"
-                                    src={`https://cdn.discordapp.com/emojis/647415388577071104.png?v=1`}
-                                    style={{ marginRight: 5 + "px" }}
-                                    title="Verified Server"
-                                ></img>
-                            ) : server.badge === "fireverified" ? (
-                                <img
-                                    height="20px"
-                                    src={`https://cdn.discordapp.com/emojis/671243744774848512.png?v=1`}
-                                    style={{ marginRight: 5 + "px" }}
-                                    title="Fire Verified Server"
-                                ></img>
-                            ) : server.badge === "premium" ? (
-                                <img
-                                    height="20px"
-                                    src={`https://cdn.discordapp.com/emojis/680519037704208466.png?v=1`}
-                                    style={{ marginRight: 5 + "px" }}
-                                    title="Premium Server"
-                                ></img>
-                            ) : (
-                                ""
-                            )}
-                            <div className={"server-name fire"}>
-                                <span>{server.name}</span>
-                            </div>
-                        </div>
-                        <div className={"server-description"} id={`${server.id}-desc`}>
-                            <span>{server.description}</span>
-                        </div>
-                        <div className={"server-member-info"}>
-                            <div className={"server-member-count"}>
-                                <div className={"server-online-dot"}></div>
-                                <div className={"server-online-text"} id={`${server.id}-online`}>
-                                    <strong>{server.online}</strong>&nbsp;Online
-                                </div>
-                            </div>
-                            <div className={"server-member-count"}>
-                                <div className={"server-offline-dot"}></div>
-                                <div id={`${server.id}-members`}>
-                                    <strong>{server.members}</strong>&nbsp;Members
-                                </div>
-                            </div>
+                    <img className={"server-splash"} src={`${server.splash}`} />
+                    <div className={"server-shadow"}></div>
+                    <img className={"server-icon"} src={`${server.icon}`} />
+                    <div className={"server-name"}>{server.name}</div>
+                    <div className={"server-member-count"}>
+                        <div className={"server-online-text"} id={`${server.id}-online`}>
+                            {server.online}&nbsp;Online
                         </div>
                     </div>
                 </div>
