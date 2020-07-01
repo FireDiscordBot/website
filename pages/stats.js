0599b7a53fe498370ee6c73310ee14ab3f6d9845
Hadi Ka (Nystrex) <alcacounihadi@gmail.com>
2020-07-01 17:11:42 -0400

    search bar for commands page

--- pages/stats.js
+++ pages/stats.js
@@ -6,69 +6,63 @@ import Nav from "../components/Nav";
 import Footer from "../components/Footer";
 import Time from "../components/Time";
 
-
-const Stats = props => {
-    return (
-        <div>
-            <Head title={"Stats"} />
-            <Nav />
-
-            <div id="content-section">
-                <div className="container text-center">
-                    <div className={"row d-flex justify-content-center "}>
-                        <div className={"col-md-6 about-text"}>
-                            <h3>
-                                <strong>Bot Statistics</strong>
-                            </h3>{" "}
-                            <h4>
-                                <span className={"stats-prefix"}>Uptime:</span>{" "}
-                                <Time date={props.stats.uptime} />
-                            </h4>
-                            <h4>
-                                <span className={"stats-prefix"}>CPU:</span>{" "}
-                                {props.stats.cpu}
-                            </h4>
-                            <h4>
-                                <span className={"stats-prefix"}>RAM:</span>{" "}
-                                {props.stats.ram}
-                            </h4>
-                            <h4>
-                                <span className={"stats-prefix"}>Version:</span>{" "}
-                                {props.stats.version}
-                            </h4>
-                            <h4>
-                                <span className={"stats-prefix"}>Guilds:</span>{" "}
-                                {formatNumber(props.stats.guilds)}
-                            </h4>
-                            <h4>
-                                <span className={"stats-prefix"}>Users:</span>{" "}
-                                {formatNumber(props.stats.users)}
-                            </h4>
-                            <h4>
-                                <span className={"stats-prefix"}>
-                                    Commands:
-                                </span>{" "}
-                                {props.stats.commands}
-                            </h4>
-                        </div>
-                    </div>
-                </div>
+const Stats = (props) => {
+  return (
+    <div>
+      <Head title={"Stats"} />
+      <Nav />
+      <div id="content-section">
+        <div className="container text-center">
+          <div className={"row d-flex justify-content-center "}>
+            <div className={"col-md-6 about-text"}>
+              <h3>
+                <strong>Bot Statistics</strong>
+              </h3>{" "}
+              <h4>
+                <span className={"stats-prefix"}>Uptime:</span>{" "}
+                <Time date={props.stats.uptime} />
+              </h4>
+              <h4>
+                <span className={"stats-prefix"}>CPU:</span> {props.stats.cpu}
+              </h4>
+              <h4>
+                <span className={"stats-prefix"}>RAM:</span> {props.stats.ram}
+              </h4>
+              <h4>
+                <span className={"stats-prefix"}>Version:</span>{" "}
+                {props.stats.version}
+              </h4>
+              <h4>
+                <span className={"stats-prefix"}>Guilds:</span>{" "}
+                {formatNumber(props.stats.guilds)}
+              </h4>
+              <h4>
+                <span className={"stats-prefix"}>Users:</span>{" "}
+                {formatNumber(props.stats.users)}
+              </h4>
+              <h4>
+                <span className={"stats-prefix"}>Commands:</span>{" "}
+                {props.stats.commands}
+              </h4>
             </div>
-            <Footer/>
+          </div>
         </div>
-    );
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
-Stats.getInitialProps = async function() {
-    const res = await fetch("https://api.gaminggeek.dev/stats");
-    const data = await res.json();
-    return {
-        stats: data
-    };
+Stats.getInitialProps = async function () {
+  const res = await fetch("https://api.gaminggeek.dev/stats");
+  const data = await res.json();
+  return {
+    stats: data,
+  };
 };
 
 function formatNumber(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 export default Stats;
