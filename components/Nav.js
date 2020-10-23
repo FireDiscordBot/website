bdab6a7c4a4acd93a816ce82c6564cf2abbf4d6d
Bruno Henrique Paiva <bruno@sigane.com.br>
2020-10-23 09:11:43 -0300

    Use next/link to take advantage of next's client-side route transitions

--- components/Nav.js
+++ components/Nav.js
@@ -1,50 +1,62 @@
 import React from "react";
+import Link from 'next/link';
+
+const NavLink = ({ href, children }) => (
+    <Link href={href} passHref>
+        <a className="nav-link">
+            {children}
+        </a>
+    </Link>
+);
+
 const Nav = () => (
-	<nav className="navbar navbar-dark navbar-expand-md" id="navbar">
-		<div className="container">
-			<a className="navbar-brand" href="/" style={{ fontSize: 14 + "pt" }}>
-				<img id="logo-img" src={"/logo.svg"} />
-			</a>
-			<button data-toggle="collapse" className="navbar-toggler" data-target="#navcol-1">
-				<span className="sr-only">Toggle navigation</span>
-				<span className="navbar-toggler-icon"></span>
-			</button>
-			<div className="collapse navbar-collapse" id="navcol-1">
-				<ul className="nav navbar-nav ml-auto">
-					<li className="nav-item" role="presentation">
-						<a className="nav-link" href="/discover">
-							Public Servers
-						</a>
-					</li>
-					<li className="nav-item" role="presentation">
-						<a className="nav-link" href="/commands">
-							Commands
-						</a>
-					</li>
-					<li className="nav-item" role="presentation">
-						<a className="nav-link" href="/stats">
-							Stats
-						</a>
-					</li>
-					<li className="nav-item" role="presentation">
-						<a className="nav-link" href="https://inv.wtf/fire">
-							Support
-						</a>
-					</li>
-					<li className="nav-item" role="presentation">
-						<a className="nav-link" href="https://github.com/FireDiscordBot">
-							GitHub
-						</a>
-					</li>
-					<li className="nav-item" role="presentation">
-						<a className="nav-link" href="/about">
-							About
-						</a>
-					</li>
-				</ul>
-			</div>
-		</div>
-	</nav>
+    <nav className="navbar navbar-dark navbar-expand-md" id="navbar">
+        <div className="container">
+            <Link href="/">
+                <a className="navbar-brand" style={{fontSize: '14pt'}}>
+                    <img id="logo-img" src="/logo.svg"/>
+                </a>
+            </Link>
+            <button data-toggle="collapse" className="navbar-toggler" data-target="#navcol-1">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="navbar-toggler-icon"/>
+            </button>
+            <div className="collapse navbar-collapse" id="navcol-1">
+                <ul className="nav navbar-nav ml-auto">
+                    <li className="nav-item" role="presentation">
+                        <NavLink href="/discover">
+                            Public Servers
+                        </NavLink>
+                    </li>
+                    <li className="nav-item" role="presentation">
+                        <NavLink href="/commands">
+                            Commands
+                        </NavLink>
+                    </li>
+                    <li className="nav-item" role="presentation">
+                        <NavLink href="/stats">
+                            Stats
+                        </NavLink>
+                    </li>
+                    <li className="nav-item" role="presentation">
+                        <a className="nav-link" href="https://inv.wtf/fire">
+                            Support
+                        </a>
+                    </li>
+                    <li className="nav-item" role="presentation">
+                        <a className="nav-link" href="https://github.com/FireDiscordBot">
+                            GitHub
+                        </a>
+                    </li>
+                    <li className="nav-item" role="presentation">
+                        <NavLink href="/about">
+                            About
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 );
 
 export default Nav;
