7f0b774b67646aa3036b25261682c50b965fd13c
Jake Ward <geek@gaminggeek.dev>
2020-12-26 00:26:13 +0000

    change to cf analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,38 +2,33 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 import React from "react";
 
 class MyDocument extends Document {
-	render() {
-		return (
-			<Html>
-				<Head>
-					<link href="/favicon.ico" rel="icon" />
-					<link href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" rel="stylesheet" />
-				</Head>
-				<body>
-					<Main />
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link href="/favicon.ico" rel="icon" />
+          <link
+            href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
 
-					<script src="https://www.googletagmanager.com/gtag/js?id=UA-154645451-2" async />
-					<script
-						dangerouslySetInnerHTML={{
-							__html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'UA-154645451-2', {
-              page_path: window.location.pathname,
-            });
-          `,
-						}}
-					/>
-					<script src="https://cdn.polyfill.io/v2/polyfill.min.js"></script>
-					<script src="https://unpkg.com/@webcomponents/webcomponentsjs@2.1.3/webcomponents-bundle.js"></script>
-					<script src="https://unpkg.com/@statuspage/status-widget/dist/index.js"></script>
+          <script
+            defer
+            src="https://static.cloudflareinsights.com/beacon.min.js"
+            data-cf-beacon='{"token": "22fdececdce24b6dbb76f5eb5da10417"}'
+          ></script>
+          <script src="https://cdn.polyfill.io/v2/polyfill.min.js"></script>
+          <script src="https://unpkg.com/@webcomponents/webcomponentsjs@2.1.3/webcomponents-bundle.js"></script>
+          <script src="https://unpkg.com/@statuspage/status-widget/dist/index.js"></script>
 
-					<NextScript />
-				</body>
-			</Html>
-		);
-	}
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
 export default MyDocument;
