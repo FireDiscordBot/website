c5783c03f6e8f43bef50fefb3fe5708d68b34816
Bruno Henrique Paiva <bruno@sigane.com.br>
2021-02-04 14:55:45 -0300

    Removed duplicated charset meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,6 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta name="theme-color" content={theme.palette.primary.main}/>
-          <meta charSet="utf-8"/>
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
