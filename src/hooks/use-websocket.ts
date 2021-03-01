67be48d1952ef08b09e8d688f6c25017da887b57
Jake Ward <geek@gaminggeek.dev>
2021-03-01 00:38:43 +0000

    make useWebsocket decode message and emit

--- src/hooks/use-websocket.ts
+++ src/hooks/use-websocket.ts
@@ -1,16 +1,31 @@
+import { EventEmitter } from "events"
+
 import * as React from "react"
 
-const useWebsocket = (url: string) => {
-  const [lastMessage, setLastMessage] = React.useState<MessageEvent | null>(null)
+import { MessageUtil } from "@/lib/ws/message-util"
+import { WebsiteEvents } from "@/interfaces/aether"
 
+const useWebsocket = (url: string, emitter: EventEmitter) => {
+  const [ws, setWebSocket] = React.useState<WebSocket | null>(null)
   React.useEffect(() => {
     const ws = new WebSocket(url)
-    ws.onmessage = (message) => setLastMessage(message)
+    ws.onmessage = (message) => {
+      const decoded = MessageUtil.decode(message.data)
+      if (!decoded) return
+      console.debug(
+        `%c WS %c ${WebsiteEvents[decoded.type]}`,
+        "background: #279AF1; color: white; border-radius: 3px 0 0 3px;",
+        "background: #2F2F2F; color: white; border-radius: 0 3px 3px 0",
+        decoded.data,
+      )
+      emitter.emit(WebsiteEvents[decoded.type], decoded.data)
+    }
+    setWebSocket(ws)
 
     return () => ws.close()
-  }, [url])
+  }, [emitter, url])
 
-  return [lastMessage]
+  return [ws]
 }
 
 export default useWebsocket
