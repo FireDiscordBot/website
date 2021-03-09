735682bf98cc1ee580b930cc8a425286d2c3fffe
Jake Ward <geek@gaminggeek.dev>
2021-03-09 19:02:17 +0000

    return error body on NetworkError

--- src/pages/api/user/reminders/create.ts
+++ src/pages/api/user/reminders/create.ts
@@ -3,14 +3,23 @@ import { StatusCodes } from "http-status-codes"
 import { createUserReminder } from "@/lib/aether"
 import { AuthenticatedApiHandler } from "@/types"
 import { error, withSession } from "@/utils/api-handler-utils"
+import { NetworkError } from "@/utils/fetcher"
 
 const handler: AuthenticatedApiHandler = async (session, req, res) => {
   if (req.method !== "POST") {
     error(res, StatusCodes.METHOD_NOT_ALLOWED)
     return
+  } else if (!req.body.reminder || req.body.timestamp < +new Date()) {
+    error(res, StatusCodes.BAD_REQUEST)
+    return
   }
-  const reminder = await createUserReminder(session.accessToken, req.body).catch(() => null)
-  if (reminder != null) res.status(201)
+
+  const reminder = await createUserReminder(session.accessToken, req.body).catch((e) => e)
+  if (reminder instanceof NetworkError)
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return res.status(reminder.code).json(reminder.data as any)
+  res.json(req.body)
+  return
 }
 
 export default withSession(handler)
