ae07a9cf36c1168e72be4e8311acdd772513f525
Jake Ward <geek@gaminggeek.dev>
2021-03-16 15:09:55 +0000

    use .catch instead

--- src/lib/ws/event-handler.ts
+++ src/lib/ws/event-handler.ts
@@ -130,8 +130,8 @@ export class EventHandler {
           "background: #353A47; color: white; border-radius: 0 3px 3px 0",
         )
         // Failed to verify identify, check session
-        try {
-          getSession().then(async (session) => {
+        getSession()
+          .then(async (session) => {
             if (session && session.accessToken) {
               const user = await fetchUser(session.accessToken).catch()
               if (!user && typeof window !== "undefined") {
@@ -152,14 +152,14 @@ export class EventHandler {
               }
             }
           })
-        } catch {
-          console.warn(
-            `%c Session %c Session is invalid or failed to fetch, attempting to logout `,
-            "background: #FFFD98; color: black; border-radius: 3px 0 0 3px;",
-            "background: #353A47; color: white; border-radius: 0 3px 3px 0",
-          )
-          window.document.getElementById("user-menu-logout")?.click()
-        }
+          .catch(() => {
+            console.warn(
+              `%c Session %c Session is invalid or failed to fetch, attempting to logout `,
+              "background: #FFFD98; color: black; border-radius: 3px 0 0 3px;",
+              "background: #353A47; color: white; border-radius: 0 3px 3px 0",
+            )
+            window.document.getElementById("user-menu-logout")?.click()
+          })
       }
       // cannot recover from codes below (4001 is special and handled above but if we're here, it didn't work)
       if (event.code == 1013 || event.code == 1008 || event.code == 4001 || event.code == 4015 || event.code == 4016)
