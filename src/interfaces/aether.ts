884abd6de5244aa4ac8b18512adc6690c047bf62
Jake Ward <geek@gaminggeek.dev>
2021-03-28 17:44:50 +0100

    payload can now include nonce

--- src/interfaces/aether.ts
+++ src/interfaces/aether.ts
@@ -90,10 +90,11 @@ export type UserGuild = DiscordGuild & {
 }
 
 export type Payload = {
-  op: number
-  d?: unknown
-  s?: number
-  t?: string
+  op: number // opcode
+  d?: unknown // data
+  s?: number // sequence
+  t?: string // type as UPPER_SNAKE_CASE
+  n?: string // nonce
 }
 
 export enum WebsiteEvents {
