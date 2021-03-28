6e538780b15ac65a0c7708895c7cb852d4aaadfe
Jake Ward <geek@gaminggeek.dev>
2021-03-28 17:44:39 +0100

    construct socket after event handler

--- src/hooks/use-websocket.ts
+++ src/hooks/use-websocket.ts
@@ -4,20 +4,33 @@ import * as React from "react"
 import { getSession } from "next-auth/client"
 
 import { EventHandler } from "@/lib/ws/event-handler"
+import { Websocket } from "@/lib/ws/websocket"
+import { fire } from "@/constants"
 
-const useWebsocket = (url: string, emitter: EventEmitter) => {
+const useWebsocket = (emitter: EventEmitter) => {
   const [handler, setHandler] = React.useState<EventHandler | null>(null)
   React.useEffect(() => {
-    const ws = new WebSocket(url)
+    let ws: Websocket
     const initHandler = async () => {
       const session = await getSession()
-      const handler = new EventHandler(session, emitter).setWebsocket(ws)
+      const handler = new EventHandler(session, emitter)
+      ws = new Websocket(
+        typeof window == "undefined"
+          ? fire.websiteSocketUrl
+          : `${fire.websiteSocketUrl}?sessionId=${window.localStorage.getItem(
+              "aether_session",
+            )}&seq=${window.localStorage.getItem("aether_seq")}`,
+        handler,
+      )
+      handler.setWebsocket(ws)
+      if (typeof window != "undefined" && window.localStorage.getItem("aether_session"))
+        handler.session = window.localStorage.getItem("aether_session") as string
       setHandler(handler)
     }
     initHandler()
 
-    return () => ws.close()
-  }, [emitter, url])
+    return () => ws?.close()
+  }, [emitter])
 
   return [handler]
 }
