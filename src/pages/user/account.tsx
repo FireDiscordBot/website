202abe00f14485ad97cc37ec023ba48931050abb
Jake Ward <geek@gaminggeek.dev>
2021-04-03 05:29:02 +0100

    changes to requesting data

--- src/pages/user/account.tsx
+++ src/pages/user/account.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 import Link from "next/link"
 import Box from "@material-ui/core/Box"
 import Button from "@material-ui/core/Button"
+import Tooltip from "@material-ui/core/Tooltip"
 import Avatar from "@material-ui/core/Avatar"
 import Typography from "@material-ui/core/Typography"
 import Card from "@material-ui/core/Card"
@@ -9,18 +10,28 @@ import CardContent from "@material-ui/core/CardContent"
 import CardActions from "@material-ui/core/CardActions"
 import { createStyles, makeStyles } from "@material-ui/core/styles"
 import Skeleton from "@material-ui/lab/Skeleton"
+import useSWR from "swr"
 
-import { emitter } from "../_app"
+import { emitter, handler } from "../_app"
 
-import { openUrl } from "@/utils/open-url"
-import fetcher, { createErrorResponse } from "@/utils/fetcher"
-import { PostCollectData } from "@/types"
 import UserPageLayout from "@/layouts/user-page"
 import Loading from "@/components/loading"
 import DiscordFlagImage from "@/components/DiscordFlagImage"
 import useSession from "@/hooks/use-session"
 import useCurrentSubscription from "@/hooks/use-current-subscription"
 import { parseFlags } from "@/utils/discord"
+import { GetCollectData } from "@/types"
+
+type DataRequestResponse =
+  | { success: false; error: string }
+  | {
+      success: true
+      url: string
+      data: {
+        type: "Buffer"
+        data: number[]
+      }
+    }
 
 const useStyles = makeStyles((theme) =>
   createStyles({
@@ -59,6 +70,11 @@ const AccountPage = () => {
       autoHideDuration: 5000,
     })
 
+  const { data: dataRequest, revalidate } = useSWR<GetCollectData>(session ? "/api/user/collect-data" : null, {
+    revalidateOnReconnect: false,
+    revalidateOnFocus: false,
+  })
+
   React.useEffect(() => {
     setErrorMessage(error?.message)
   }, [error])
@@ -72,19 +88,72 @@ const AccountPage = () => {
 
   const onClickRequestData = async (event: React.MouseEvent) => {
     event.preventDefault()
+    await revalidate()
 
-    let json: PostCollectData
-    try {
-      json = await fetcher<PostCollectData>("/api/user/collect-data", {
-        method: "POST",
-      })
-    } catch (e) {
-      setErrorMessage(createErrorResponse(e).error)
-      return
+    if (dataRequest && dataRequest.status != 0) {
+      const link = document.createElement("a")
+      link.href = dataRequest.url
+      link.download = `${handler.session}.zip`
+      link.click()
+      return link.remove()
     }
 
-    openUrl(json.url, true, true)
+    // let json: PostCollectData
+    // try {
+    //   json = await fetcher<PostCollectData>("/api/user/collect-data", {
+    //     method: "POST",
+    //   })
+    // } catch (e) {
+    //   setErrorMessage(createErrorResponse(e).error)
+    //   return
+    // }
+
+    // openUrl(json.url, true, true)
+
+    const dataResponse = await requestData().catch(() => {
+      emitter.emit("NOTIFICATION", {
+        text: "Failed to request data",
+        severity: "error",
+        horizontal: "right",
+        vertical: "top",
+        autoHideDuration: 5000,
+      })
+      return null
+    })
+    if (!dataResponse) return
+    if (!dataResponse.success)
+      return emitter.emit("NOTIFICATION", {
+        text: dataResponse.error,
+        severity: "error",
+        horizontal: "right",
+        vertical: "top",
+        autoHideDuration: 5000,
+      })
+    else {
+      const link = document.createElement("a")
+      const file = new Blob([Buffer.from(dataResponse.data)], { type: "application/zip" })
+      // link.href = dataResponse.url
+      link.href = URL.createObjectURL(file)
+      link.download = `${handler.session}.zip`
+      link.click()
+      link.remove()
+    }
   }
+
+  const requestData = async (): Promise<DataRequestResponse> =>
+    new Promise((resolve, reject) => {
+      if (!handler) return resolve({ success: false, error: "NO_HANDLER" })
+      const nonce = (+new Date()).toString()
+      handler.websocket?.handlers.set(nonce, resolve)
+      handler.requestData(nonce)
+
+      setTimeout(() => {
+        if (handler.websocket?.handlers.has(nonce)) {
+          handler.websocket.handlers.delete(nonce)
+          reject("Data request timed out")
+        }
+      }, 10000)
+    })
 
   return (
     <UserPageLayout title="Account" noindex nofollow>
@@ -108,9 +177,29 @@ const AccountPage = () => {
           </Box>
         </CardContent>
         <CardActions>
-          <Button color="primary" onClick={onClickRequestData}>
-            Request collected data
-          </Button>
+          {dataRequest && dataRequest.status != 0 && (
+            <Tooltip
+              title={
+                dataRequest.last_request
+                  ? `You've recently requested a copy of your data. You can request again on ${new Date(
+                      dataRequest.last_request + 2592000000,
+                    ).toDateString()}`
+                  : "You've recently requested a copy of your data. You cannot request again at this time."
+              }
+            >
+              <span>
+                <Button color="primary" disabled>
+                  Request collected data
+                </Button>
+              </span>
+            </Tooltip>
+          )}
+          {!dataRequest ||
+            (dataRequest && dataRequest.status == 0 && (
+              <Button color="primary" onClick={onClickRequestData}>
+                Request collected data
+              </Button>
+            ))}
         </CardActions>
       </Card>
 
