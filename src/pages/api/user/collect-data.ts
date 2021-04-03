f7709018a85c5db6a464edd559b499d4819daef6
Jake Ward <geek@gaminggeek.dev>
2021-04-03 05:28:17 +0100

    update /api/user/collect-data

--- src/pages/api/user/collect-data.ts
+++ src/pages/api/user/collect-data.ts
@@ -1,18 +1,22 @@
 import { StatusCodes } from "http-status-codes"
 
-import { createDataArchive } from "@/lib/aether"
-import { AuthenticatedApiHandler, PostCollectData } from "@/types"
+import { createDataArchive, getDataRequest } from "@/lib/aether"
+import { AuthenticatedApiHandler, GetCollectData, PostCollectData } from "@/types"
 import { error, withSession } from "@/utils/api-handler-utils"
 
-const handler: AuthenticatedApiHandler<PostCollectData> = async (session, req, res) => {
-  if (req.method !== "POST") {
+const handler: AuthenticatedApiHandler<PostCollectData | GetCollectData> = async (session, req, res) => {
+  if (req.method !== "POST" && req.method !== "GET") {
     error(res, StatusCodes.METHOD_NOT_ALLOWED)
     return
   }
 
-  const url = await createDataArchive(session.accessToken)
-
-  res.json({ url })
+  if (req.method === "POST") {
+    const url = await createDataArchive(session.accessToken)
+    res.json({ url })
+  } else if (req.method === "GET") {
+    const data = await getDataRequest(session.accessToken)
+    res.json(data)
+  }
 }
 
 export default withSession(handler)
