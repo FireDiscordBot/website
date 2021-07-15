233e82b8a6705904db07e1bbe863472091edd9fa
Jake Ward <geek@gaminggeek.dev>
2021-07-15 08:50:04 +0100

    specify which limit in logging, add notification for global

--- src/hooks/use-websocket.ts
+++ src/hooks/use-websocket.ts
@@ -19,7 +19,9 @@ const useWebsocket = (emitter: EventEmitter) => {
       const gateway = await handler.api.gateway.website.get<WebsiteGateway>({ version: 2 })
       if (!gateway.limits.connect.remaining) {
         console.error(
-          `%c WS %c Rate Limit %c Waiting for ${gateway.limits.connect.resetAfter / 1000} seconds... `,
+          `%c WS %c Rate Limit %c Hit WS connect limit, waiting for ${
+            gateway.limits.connect.resetAfter / 1000
+          } seconds... `,
           "background: #279AF1; color: white; border-radius: 3px 0 0 3px;",
           "background: #C95D63; color: white; border-radius: 3px 0 0 3px;",
           "background: #353A47; color: white; border-radius: 0 3px 3px 0",
@@ -27,11 +29,21 @@ const useWebsocket = (emitter: EventEmitter) => {
         await sleep(gateway.limits.connect.resetAfter)
       } else if (!gateway.limits.connectGlobal.remaining) {
         console.error(
-          `%c WS %c Rate Limit %c Waiting for ${gateway.limits.connectGlobal.resetAfter / 1000} seconds... `,
+          `%c WS %c Rate Limit %c Hit global WS connect limit, waiting for ${
+            gateway.limits.connectGlobal.resetAfter / 1000
+          } seconds... `,
           "background: #279AF1; color: white; border-radius: 3px 0 0 3px;",
           "background: #C95D63; color: white; border-radius: 3px 0 0 3px;",
           "background: #353A47; color: white; border-radius: 0 3px 3px 0",
         )
+        // this may be a long time (as the limit is currently per-day) so we send a notification
+        handler.emitter.emit("NOTIFICATION", {
+          text: "You are being rate limited!",
+          severity: "error",
+          horizontal: "right",
+          vertical: "top",
+          autoHideDuration: gateway.limits.connectGlobal.resetAfter,
+        })
         await sleep(gateway.limits.connectGlobal.resetAfter)
       }
       ws = new Websocket(
