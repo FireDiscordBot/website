3b03ab24fcb3ba7ed1e99ae6280ad5d61cc89aa2
Jake Ward <geek@gaminggeek.dev>
2021-12-18 22:01:20 +0000

    fix types

--- src/utils/api-handler-utils.ts
+++ src/utils/api-handler-utils.ts
@@ -3,6 +3,7 @@ import { NextApiResponse } from "next"
 import { getSession } from "next-auth/client"
 
 import { ApiErrorResponse, ApiHandler, ApiResponseBody, AuthenticatedApiHandler } from "@/types"
+import { AuthSession } from "@/interfaces/auth"
 
 export const error = <T>(res: NextApiResponse<ApiResponseBody<T>>, code: StatusCodes, message?: string) => {
   if (message?.startsWith(`${code}: `)) message = message.slice(code.toString().length + 2)
@@ -15,13 +16,13 @@ export const error = <T>(res: NextApiResponse<ApiResponseBody<T>>, code: StatusC
 
 export const withSession = <T>(handler: AuthenticatedApiHandler<T>): ApiHandler<T> => {
   return async (req, res) => {
-    const session = await getSession({ req })
+    const session = (await getSession({ req })) as AuthSession
     if (!session?.accessToken) {
       error(res, StatusCodes.UNAUTHORIZED)
       return
     }
 
-    const validSession = { ...session, accessToken: session.accessToken }
+    const validSession = { ...session, accessToken: session.accessToken as string }
 
     await handler(validSession, req, res)
   }
