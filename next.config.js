cc4fd8979e5ba59ea8236a42f0f0de260cfeec20
Jake Ward <geek@gaminggeek.dev>
2022-01-16 18:40:54 +0000

    Revert "IT WORKS"
    This reverts commit 9e8b023d345ca178467745110f9516e2d2d0ae3b.

--- next.config.js
+++ next.config.js
@@ -2,10 +2,4 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 })
 
-module.exports = {
-  experimental: {
-    swcFileReading: false
-  }
-}
-
 module.exports = withBundleAnalyzer({})
