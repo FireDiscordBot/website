19137ffbd3e19e8b4fc0a64fc3ca87bd4b83bdf6
Hadi Ka (Nystrex) <alcacounihadi@gmail.com>
2022-01-17 09:38:15 -0500

    forgot to del usless shit that was on stackoverflo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -35,21 +35,13 @@ const StyledAppBar = styled(AppBar, {
 const NavBar = () => {
   const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null)
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null)
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null)
 
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget)
   }
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget)
-  }
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null)
-  }
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null)
   }
 
   const scrollTrigger = useScrollTrigger({
